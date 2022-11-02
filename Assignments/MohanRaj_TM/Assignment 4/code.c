245e86dcbebdac46ebc60731908fe0a97f62807f
hemamalini110 <85781362+hemamalini110@users.noreply.github.com>
2022-11-01 22:22:20 -0700

    Create code.c

---
+++ Assignments/MohanRaj_TM/Assignment 4/code.c
@@ -0,0 +1,19 @@
+#include “Ultrasonic.h"
+Ultrasonic ultrasonic(6,2);
+int distance; void setup()
+{
+Serial.begin(9600);
+}
+void loop() {
+distance = ultrasonic.read(CM); Serial.print("Distance
+in CM: “);
+Date 29th October 2022
+Team ID PNT2022TMID47485
+Roll No 910019106032
+Project Name Project:- Signs With Smart Connectivity For Better
+Safety
+Serial.println(distance); if
+(distance < 100)
+Serial.print("alert");
+Serial.println(); delay(3000);
+}
